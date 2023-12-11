1dbe64669e72b31a06552336b6cdd2a0d628db0c
Nguyen Ha <hadimsctn@gmail.com>
2023-12-11 18:14:27 +0700

    update create order

--- backend/src/order/orderService.js
+++ backend/src/order/orderService.js
@@ -14,13 +14,21 @@ module.exports.getDataFromDBService = () => {
 module.exports.createOrderDBService = (orderDetails) => {
     return new Promise((resolve, reject) => {
         var orderModelData = new orderModel()
-        orderModelData.product_id = orderDetails.product_id
+        
         orderModelData.order_purchase_timestamp = orderDetails.order_purchase_timestamp
-        orderModelData.price = orderDetails.price
-        orderModelData.product_category_name = orderDetails.product_category_name
         orderModelData.customer_id = orderDetails.customer_id
 
-
+        
+        for (let i = 0; i < orderDetails.products.length; i++) {
+            const productData = {
+              product_id: orderDetails.products[i].product_id,
+              price: orderDetails.products[i].price,
+              product_category_name: orderDetails.products[i].product_category_name,
+              quantity: orderDetails.products[i].quantity
+            };
+          
+            orderModelData.products.push(productData);
+        }
         orderModelData.save().then(result => {
             console.log("Lưu dữ liệu thành công!")
             resolve(result);
